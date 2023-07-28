620f9ffaf7f58ac8deeb4831ec4922c9f9fa14af
MatheusCosta-GitHub <109094304+MatheusCosta-GitHub@users.noreply.github.com>
2023-07-28 14:47:51 -0300

    Modal funcional e inteligente
    O modal agora tem informações dinâmicas que vem diretamente dos inputs que o usuário digitou.

--- modal.js
+++ modal.js
@@ -1,6 +1,55 @@
 // esse arquivo serve para colocar inteligência no modal da página
-const overlay = document.getElementById('modal');
 const background = document.getElementById('modal-background');
+const modalContainer = document.getElementById('modal-container');
+
+
+//criando a função que cria o modal:
+function createModal(data) {
+    modalContainer.innerHTML= `                
+        <div id="movie-title">
+            <h2>${data.Title}</h2>
+        </div>
+
+        <section id="modal-body">
+
+            <img id="movie-poster"
+                src="${data.Poster}"
+                alt="Poster do filme" />
+
+            <div id="movie-info">
+
+                <h3 id="movie-plot">
+
+                    ${data.Plot}
+
+                </h3>
+
+                <div id="movie-cast">
+
+                    <h4>Elenco:</h4>
+                    <h5>${data.Actors}</h5>
+
+                </div>
+
+                <div id="movie-genre">
+
+                    <h4>Gênero:</h4>
+                    <h5>${data.Genre}</h5>
+
+                </div>
+
+            </div>
+
+        </section>
+
+        <section id="modal-footer">
+
+            <button id="add-to-list">Adicionar a lista</button>
+
+        </section>
+
+        </section>`
+}
 
 //função que será chamada quando clicar:
 function backgroundClickHandler() {
--- script.js
+++ script.js
@@ -1,13 +1,59 @@
 // esse arquivo é responsável pela inteligência da página com um todo
-
+const overlay = document.getElementById('modal');
 const searchButton = document.getElementById('search-button');
+const movieName = document.getElementById('movie-name');
+const movieYear = document.getElementById('movie-year');
 
+// função que confere se a caixa de nome do filme está vazia ou não:
+function adaptMovieName() {
 
-function searchButtonClickHandler(){
-    //atribuindo o elemento css que deixa o elemento visível:
-    overlay.classList.add('modal-open');
+    if (movieName.value === ''){
+
+        //retorna um erro: 
+        throw new Error('O nome do filme deve ser informado!');
+    }
+    return movieName.value.split(' ').join('+');
 }
 
+//função que confere se a caixa de ano está vazia ou não:
+function adaptMovieYear(){
+    // tratando os dados enviados no input do ano do filme:
+    if (movieYear.value === ''){
+        return '';
+    }
+    
+    if(movieYear.value.length !== 4 || Number.isNaN(Number(movieYear.value))) {
+        throw new Error('Ano do filme inválido!');
+    }
 
-// criando uma ação para quando clicar sobre o botão de pesquisa:
+    return `&y=${movieYear.value}`;
+}
+
+async function searchButtonClickHandler() {
+
+    try{
+        //atribuindo o elemento css que deixa o elemento visível:
+        overlay.classList.add('modal-open');
+        let url = `http://www.omdbapi.com/?apikey=441b6882&t=${adaptMovieName()}${adaptMovieYear()}`;
+
+        const response = await fetch(url);
+        const data = await response.json();
+
+        if (data.Error){
+            throw new Error('Filme não encontrado!');
+        }
+
+        createModal(data); 
+        overlay.classList.add('modal-open');
+
+    } catch (error) {
+        notie.alert({ 
+            type: 'error',
+            text: error.message
+        });
+    }
+
+}
+
+//criando uma ação para quando clicar sobre o botão de pesquisa:
 searchButton.addEventListener('click', searchButtonClickHandler);
